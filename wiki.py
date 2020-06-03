53a494834adb2c1f0382fb1249836462604ca55e
burnt776 <burnt776@gmail.com>
2020-06-04 02:35:50 +0530

    getting the paragraph

--- wiki.py
+++ wiki.py
@@ -1,13 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-def wiki():
-    res = requests.get("http://www.wikipedia.com")
-    soup = BeautifulSoup(res.text, 'lxml')
-    titl= soup.select('title')
-    print(titl[0].text)
+import urllib.request
+from urllib.request import urlopen
 
+
+def wiki(search):
+    count=0
+    url = "https://en.wikipedia.org/wiki/"+search
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    
+
+
+
+    ele = soup.find('div', id='mw-content-text')
+    for para in ele:
+        count = count+1
+        para = ele.find_all('p')[count]
+        result = para
+        getit = para.text
+        getit = getit.split(" ")
+        len_of_string= len(getit)
+        for getit in range(0,len_of_string):
+            if(getit == search):
+                break
+    print(result.text)
 
 
 
 if __name__ == '__main__':
-    wiki()
+    search = "bird"
+    wiki(search)
