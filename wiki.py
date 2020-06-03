a5f3809edce5f590b3c49a16ce123b712eb5d714
burnt776 <burnt776@gmail.com>
2020-06-03 21:36:59 +0530

    getting title of the wikipedia

--- wiki.py
+++ wiki.py
@@ -1,5 +1,13 @@
 import requests
-
+from bs4 import BeautifulSoup
 def wiki():
-    res = requests.get("http://www.google.com")
-    print()
+    res = requests.get("http://www.wikipedia.com")
+    soup = BeautifulSoup(res.text, 'lxml')
+    titl= soup.select('title')
+    print(titl[0].text)
+
+
+
+
+if __name__ == '__main__':
+    wiki()
