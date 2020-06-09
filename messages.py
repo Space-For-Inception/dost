d18f8ed58797b4abf16ef99151339d778e895575
Shreyas Ashtamkar <shreyu@programmer.net>
2020-06-09 11:37:11 +0530

    Optimised Code, Removed Errors

--- messages.py
+++ messages.py
@@ -1,11 +1,23 @@
 Main_Menu = """
-This is a Whatsapp Chatbot, Built by Shreyas Ashtamkar. The services I can offer are :- 
+This is a Whatsapp Chatbot, Built by Space-For-Inception . The services I can offer are :- 
 These are the following services I can offer.: 
-1. Dictionary App -- 
-    Get the Song Video which you Like
-    example : 
-
-        song Daddy Cool
+1. *Wikipedia* -- 
+    Get the wikipedia Info.
+    
+    _example : 
+        wiki India_
+        
+2. *Covid-19* -- 
+    Get current numbers of Covid-19 situation of specific country.
+    
+    _example : 
+        covid India
+        
+3. *Youtube Video* -- 
+    Search a video for what you wish to do
+    
+    _example : 
+        youtube Tom and Jerry_
 """
 
 Error_Reply = "Invalid Option. please type *info or *help for help."
