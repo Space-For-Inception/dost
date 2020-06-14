af81cbe94659ea216d18a94dd64cc36e1605e13d
Shreyas-Ashtamkar <shreyu@programmer.net>
2020-06-14 07:36:37 +0530

    Corrected Non-Returning News, now returns Text Correctly

--- news.py
+++ news.py
@@ -8,10 +8,14 @@ def news():
 
     soup = BeautifulSoup(res.content , 'html.parser')
     data = soup.find_all('article' , {'jslog':'85008'})
-    for ele in data[0:10]:
-        return ele+'\n\n'
+
+    final_news_list = ''
+
+    for ele in data[:5]:
+        final_news_list+='\n\n'+ele.text
+    
+    return "The Top 3 News of the Day are :\n\n"+final_news_list
 
 
 if __name__ == '__main__':
-    news()    
-    
+    print(news())
